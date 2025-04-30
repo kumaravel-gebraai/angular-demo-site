1016ed657b755d1c33e986238ddf825e876f2cc8
Kumaravel.P <kumaravel@gebra.ai>
2025-04-30 14:01:05 +0530

    UI Improved

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
 import { AboutComponent } from './app/about/about.component';
+import { HomeComponent } from './app/home/home.component';
+import { ServicesComponent } from './app/services/services.component';
+import { ContactComponent } from './app/contact/contact.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/about', pathMatch: 'full' }
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 bootstrapApplication(AppComponent, {
